4bc8ba4127b739aae1a39a7bf1f23c835bcf2cd9
Manuel Valls Fernández <manolo@vlrz.es>
2015-09-08 13:08:55 +0200

    Added default value and key to getValue and getkey

--- get-key.js
+++ get-key.js
@@ -6,23 +6,23 @@ function hyphenize(m){
 
 module.exports = function(key){
   var fl,ret;
-  
+
   key = key.toString();
   if(check.key(key)) return key;
-  
+
   key = key.replace(/[A-Z]/g,hyphenize);
-  
+
   ret = '-webkit-' + key;
   if(check.key(ret)) return ret;
-  
+
   ret = '-moz-' + key;
   if(check.key(ret)) return ret;
-  
+
   ret = '-ms-' + key;
   if(check.key(ret)) return ret;
-  
+
   ret = '-o-' + key;
   if(check.key(ret)) return ret;
-  
-};
 
+  return key;
+};
--- get-value.js
+++ get-value.js
@@ -6,23 +6,23 @@ function hyphenize(m){
 
 module.exports = function(key,value){
   var fl,ret;
-  
+
   value = value.toString();
   if(check(key,value)) return value;
-  
+
   value = value.replace(/[A-Z]/g,hyphenize);
-  
+
   ret = '-webkit-' + value;
   if(check(key,ret)) return ret;
-  
+
   ret = '-moz-' + value;
   if(check(key,ret)) return ret;
-  
+
   ret = '-ms-' + value;
   if(check(key,ret)) return ret;
-  
+
   ret = '-o-' + value;
   if(check(key,ret)) return ret;
-  
-};
 
+  return value;
+};
